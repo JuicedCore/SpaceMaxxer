7f74d990add1bd9516a6431ef41a910865fb3846
juicedcore <itscris7ron@gmail.com>
2026-02-12 14:28:02 +0530

    Decoupling storage from CLI_TOOL

---
+++ libs/storage/src/lib.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io::{self, BufWriter,Write};
+use std::path::Path;
+use std::process::Output;
+use crawler::types::Node;
+
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat{
+    Json, 
+    Binary,
+}
+
+pub fn save_scan(node:&Node, path:&Path, format: OutputFormat) -> io::Result<()>{
+    let file = File::create(path)?;
+    let mut writer = BufWriter::new(file);
+
+    match format{
+        OutputFormat::Json => {
+            serde_json::to_writer_pretty(&mut writer, node)?;
+            writeln!(writer)?;
+        }
+
+        OutputFormat::Binary =>{
+            bincode::serialize_into(&mut writer, node)
+            .map_err(|e| io::Error::new(io:ErrorKind::Other, e))?;
+        }
+    }
+
+    writer.flush()?;
+
+    Ok(())
+}
